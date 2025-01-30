1aaa80551a7381576582751e63d1490f914edd59
Aqif4 <aqif.choudhury@gmail.com>
2025-01-30 14:10:01 +0000

    changed plotter

--- aqif_row_plotter.py
+++ aqif_row_plotter.py
@@ -13,21 +13,25 @@ import pickle
 
 # Setting input arguments
 plot_spec = 'H2O,CH4,CO2,CO,NH3'  # Species to plot, separated by commas
-plot_name = 'Inhabited_k218_to_toi'  # Output plot name
+plot_name = 'mtol investigation, original'  # Output plot name
 plot_dir = vulcan_cfg.plot_dir  # Assume it's defined correctly
 
-# Custom axis limits
-x_min, x_max = 1.E-10, 1
-y_min, y_max = 1, 1.E-11  # Example limits; customize as needed
-
 # Path to input files
-vul_files = ['output/k218_to_toi/VIH_TOI_radius_g_GJ176_nz250_1e17s.vul',
-             'output/k218_to_toi/VIH_TOI_radius_g_orbit_GJ176_nz250_1e17s.vul',
-             'output/k218_to_toi/VIH_TOI_radius_g_orbit_GJ163_nz250_1e16s.vul',
-             'output/k218_to_toi/VIH_TOI_fullchange_GJ163_nz250_1e17s.vul']
+vul_files =['output/mtol_test/K2-18b_GJ176_nz250_1e12s_greg.vul',
+             'output/mtol_test/K2-18b_GJ176_nz250_1e12s_q.vul',
+             'output/mtol_test/K2-18b_GJ176_nz250_1e12s_10q.vul']
+             #'output/vih_botpressure/K2-18b_GJ176_nz250_1e17s_pb_5bar.vul',
+             #'output/vih_botpressure/K2-18b_GJ176_nz250_1e17s_pb_10bar.vul']
 
-titles = ['Surface gravity and Mass', 'Orbit', 'Star', 'PT profile']  # Titles for each plot
-nspec = len(plot_spec.split(','))  # Number of species
+# Titles and axis limits for each plot
+titles = ['1e-18', '1e-17', '1e-16']# '5 bar', '10 bar'
+axis_limits = [
+    {'x_min': 1.E-10, 'x_max': 1, 'y_min': 0.5, 'y_max': 1.E-11},  # Custom limits for plot 1
+    {'x_min': 1.E-10, 'x_max': 1, 'y_min': 0.5, 'y_max': 1.E-11},  # Custom limits for plot 2
+    {'x_min': 1.E-10, 'x_max': 1, 'y_min': 0.5, 'y_max': 1.E-11}#,   # Custom limits for plot 3
+    #{'x_min': 1.E-10, 'x_max': 1, 'y_min': 5, 'y_max': 1.E-11},  # Custom limits for plot 4
+    #{'x_min': 1.E-10, 'x_max': 1, 'y_min': 10, 'y_max': 1.E-11},  # Custom limits for plot 5
+]
 
 # Color setup
 tableau20 = [(31, 119, 180), (255, 127, 14), (44, 160, 44), (214, 39, 40), (148, 103, 189),
@@ -52,6 +56,9 @@ for idx, (vul_file, title) in enumerate(zip(vul_files, titles)):
     ax = axes[idx] if len(vul_files) > 1 else axes
     vulcan_spec = data['variable']['species']
 
+    # Use custom axis limits or fallback to default values
+    limits = axis_limits[idx] if idx < len(axis_limits) else {'x_min': 1.E-10, 'x_max': 1, 'y_min': 1, 'y_max': 1.E-11}
+    
     for color_index, sp in enumerate(plot_spec.split(',')):
         if color_index == len(tableau20):  # Generate random colors if limit exceeded
             tableau20.append(tuple(np.random.rand(3)))
@@ -68,8 +75,8 @@ for idx, (vul_file, title) in enumerate(zip(vul_files, titles)):
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.invert_yaxis()
-    ax.set_xlim(x_min, x_max)
-    ax.set_ylim(y_min, y_max)
+    ax.set_xlim(limits['x_min'], limits['x_max'])
+    ax.set_ylim(limits['y_min'], limits['y_max'])
     ax.legend(frameon=0, prop={'size': 10}, loc='best')
     ax.set_title(title)
     ax.set_xlabel("Mixing Ratio")
@@ -79,4 +86,3 @@ for idx, (vul_file, title) in enumerate(zip(vul_files, titles)):
 output_path = os.path.join(plot_dir, plot_name + '.png')
 plt.savefig(output_path)
 plt.show()
-
