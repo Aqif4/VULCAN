4c6d38f9449cf21f1f03c8af79f01a3d04e84b51
Aqif4 <aqif.choudhury@gmail.com>
2025-02-12 20:13:20 +0000

    Fixed error bars and made attempts at some TOI,investigated isothermal 387

--- aqif_error_plotter.py
+++ aqif_error_plotter.py
@@ -6,140 +6,137 @@ import os
 import pickle
 
 # Setting input arguments
-plot_spec = 'H2O,CH4,CO2,CO,NH3'  # Species to plot, separated by commas
-plot_name = 'mtol investigation with errors and upper bounds'  # Output plot name
+plot_spec = 'H2O,CH4,CO2,CO,NH3,C2H6'  # Species to plot, separated by commas
+plot_name = ''  # Output plot name
 plot_dir = vulcan_cfg.plot_dir  # Assume it's defined correctly
 
 # Path to input files
-vul_files = ['output/mtol_test/K2-18b_GJ176_nz250_1e12s_greg.vul',
-			 'output/mtol_test/K2-18b_GJ176_nz250_1e12s_q.vul',
-			 'output/mtol_test/K2-18b_GJ176_nz250_1e12s_10q.vul']
+vul_files = ['output/TOI_isothermal/387iso_uninhabited_5e5kzz_nz250_1e17s.vul',
+'output/TOI_isothermal/387iso_uninhabited_5e5kzz_nz250_1e17s.vul',
+'output/TOI_isothermal/387iso_Life1_5e5kzz_nz250_1e17s.vul',
+'output/TOI_isothermal/387iso_Life1_5e5kzz_nz250_1e17s.vul']
 
 # Titles and axis limits for each plot
-titles = ['1e-18', '1e-17', '1e-16']
+titles = ['Uninhabited Benneke', 'Uninhabited Madhu', 'Inhabited Benneke', 'Inhabited Madhu']
+# Specify error bar sets for each plot
+error_bar_sets_per_plot = ['benneke', 'madhu', 'benneke', 'madhu']
 axis_limits = [{'x_min': 1.E-10, 'x_max': 1, 'y_min': 0.5, 'y_max': 1.E-11}] * len(vul_files)
 
 # Color setup
 tableau20 = [(31, 119, 180), (255, 127, 14), (44, 160, 44), (214, 39, 40), (148, 103, 189),
-			 (140, 86, 75), (227, 119, 194), (127, 127, 127), (188, 189, 34), (23, 190, 207)]
+             (140, 86, 75), (227, 119, 194), (127, 127, 127), (188, 189, 34), (23, 190, 207)]
 tableau20 = [(r / 255., g / 255., b / 255.) for r, g, b in tableau20]
 
 # Define error bar sets with central x and dx values in log scale, dx values should be positive
 error_bar_sets = {
-	'benneke_toi270d': {
-		'CH4': {'x_center': -1.64, 'dx_pos': 0.38, 'dx_neg': 0.36, 'y': 0.05}, 
-		'CO2': {'x_center': -1.67, 'dx_pos': 0.40, 'dx_neg': 0.60, 'y': 0.1},
-		'H2O': {'x_center': -1.10, 'dx_pos': 0.31, 'dx_neg': 0.92, 'y': 0.01},
-		'CO': {'x_center': -1.46,'y': 0.02},
-		'NH3': {'x_center': -4.27,'y': 0.03}#,
-		#'SO2': {'x_center': 0.25e-2, 'y': 0.05},
-		#'CS2': {'x_center': 0.25e-2, 'y': 0.05}
-
-	},
-	'madhu_toi270d': {
-		'CH4': {'x_center': -2.44, 'dx_pos': 0.34, 'dx_neg': 0.46, 'y': 0.03}, 
-		'CO2': {'x_center': -1.96, 'dx_pos': 0.49, 'dx_neg': 0.79, 'y': 0.02},
-		'H2O': {'x_center': -1.56, 'y': 0.03},  
-		'NH3': {'x_center': -5.75, 'y': 0.05},
-		#'CS2': {'x_center': 0.25e-2, 'dx_pos': 0.1e-2, 'dx_neg': 0.05e-2, 'y': 0.05},
-		#'C2H6': {'x_center': 0.25e-2, 'dx_pos': 0.1e-2, 'dx_neg': 0.05e-2, 'y': 0.05},
-		'CO': {'x_center': -1.63, 'y': 0.05}
-	},
-	# You can add more sets here
+    'benneke': { #1 offset from Benneke et al. Table 2
+        'CH4': {'x_center': -1.64, 'dx_pos': 0.38, 'dx_neg': 0.36, 'y': 0.3e-3},
+        'CO2': {'x_center': -1.67, 'dx_pos': 0.40, 'dx_neg': 0.60, 'y': 0.7e-3},
+        'H2O': {'x_center': -1.10, 'dx_pos': 0.31, 'dx_neg': 0.92, 'y': 1.1e-3},
+        'CO': {'x_center': -1.46,'y': 2.2e-3},
+        'NH3': {'x_center': -4.27,'y': 1.8e-3},
+        'SO2': {'x_center': -4.39,'dx_pos': 1.01, 'dx_neg': 3.33,'y': 2.0e-3},
+        'CS2': {'x_center': -3.44,'dx_pos': 0.66, 'dx_neg': 0.67,'y': 2.2e-3}
+    },
+    'madhu': { #1 offset from madhu and Holberg 2024 Table 1
+        'CH4': {'x_center': -2.44, 'dx_pos': 0.34, 'dx_neg': 0.46, 'y': 0.5e-3},
+        'CO2': {'x_center': -1.96, 'dx_pos': 0.49, 'dx_neg': 0.79, 'y': 1.5e-3},
+        'H2O': {'x_center': -1.56, 'y': 1e-3},
+        'NH3': {'x_center': -5.75, 'y': 1.6e-3},
+        'CS2': {'x_center': -2.59, 'dx_pos': 0.67, 'dx_neg': 0.95,'y': 1.8e-3},
+        'C2H6': {'x_center': -1.23, 'y': 2.0e-3},
+        'CO': {'x_center': -1.63, 'y': 3e-3}
+    },
 }
 
-# Error bar set
-error_bar_set = 'benneke_toi270d'  
-
-# Use the selected error bar set
-species_error_data = error_bar_sets[error_bar_set]
 
 # Species color mapping
 tex_labels = {
-	'H': 'H', 'H2': 'H$_2$', 'O': 'O', 'OH': 'OH', 'H2O': 'H$_2$O',
-	'CH': 'CH', 'C': 'C', 'CH2': 'CH$_2$', 'CH3': 'CH$_3$', 'CH4': 'CH$_4$',
-	'HCO': 'HCO', 'H2CO': 'H$_2$CO', 'C4H2': 'C$_4$H$_2$', 'C2': 'C$_2$', 'C2H2': 'C$_2$H$_2$',
-	'CO': 'CO', 'CO2': 'CO$_2$', 'He': 'He', 'O2': 'O$_2$'
+    'H': 'H', 'H2': 'H$_2$', 'O': 'O', 'OH': 'OH', 'H2O': 'H$_2$O',
+    'CH': 'CH', 'C': 'C', 'CH2': 'CH$_2$', 'CH3': 'CH$_3$', 'CH4': 'CH$_4$',
+    'HCO': 'HCO', 'H2CO': 'H$_2$CO', 'C4H2': 'C$_4$H$_2$', 'C2': 'C$_2$', 'C2H2': 'C$_2$H$_2$',
+    'CO': 'CO', 'CO2': 'CO$_2$', 'He': 'He', 'O2': 'O$_2$'
 }
 
 # Plot setup
 fig, axes = plt.subplots(1, len(vul_files), figsize=(len(vul_files) * 6, 5), constrained_layout=True)
 
-for idx, (vul_file, title) in enumerate(zip(vul_files, titles)):
-	with open(vul_file, 'rb') as handle:
-		data = pickle.load(handle)
+for idx, (vul_file, title, error_bar_set) in enumerate(zip(vul_files, titles, error_bar_sets_per_plot)):
+    with open(vul_file, 'rb') as handle:
+        data = pickle.load(handle)
 
-	ax = axes[idx] if len(vul_files) > 1 else axes
-	vulcan_spec = data['variable']['species']
+    ax = axes[idx] if len(vul_files) > 1 else axes
+    vulcan_spec = data['variable']['species']
 
-	# Use custom axis limits or fallback to default values
-	limits = axis_limits[idx] if idx < len(axis_limits) else {'x_min': 1.E-10, 'x_max': 1, 'y_min': 1, 'y_max': 1.E-11}
+    # Use custom axis limits or fallback to default values
+    limits = axis_limits[idx] if idx < len(axis_limits) else {'x_min': 1.E-10, 'x_max': 1, 'y_min': 1, 'y_max': 1.E-11}
 
-	for color_index, sp in enumerate(plot_spec.split(',')):
-		if color_index == len(tableau20):  # Generate random colors if limit exceeded
-			tableau20.append(tuple(np.random.rand(3)))
-		sp_label = tex_labels.get(sp, sp)
-		if sp in vulcan_spec:
-			ax.plot(
-				data['variable']['ymix'][:, vulcan_spec.index(sp)],
-				data['atm']['pco'] / 1.e6,
-				color=tableau20[color_index],
-				label=sp_label,
-				alpha=0.9
-			)
+    for color_index, sp in enumerate(plot_spec.split(',')):
+        if color_index == len(tableau20):  # Generate random colors if limit exceeded
+            tableau20.append(tuple(np.random.rand(3)))
+        sp_label = tex_labels.get(sp, sp)
+        if sp in vulcan_spec:
+            ax.plot(
+                data['variable']['ymix'][:, vulcan_spec.index(sp)],
+                data['atm']['pco'] / 1.e6,
+                color=tableau20[color_index],
+                label=sp_label,
+                alpha=0.9
+            )
 
-		# Add error bars or upper-bound arrows matching species plot color
-		capsize = 5  # For arrows and error bars
+        # Add error bars or upper-bound arrows matching species plot color
+        capsize = 5  # For arrows and error bars
 
-		if sp in species_error_data:
-			error_data = species_error_data[sp]
+        species_error_data = error_bar_sets[error_bar_set]
+        if sp in species_error_data:
+            error_data = species_error_data[sp]
 
-			# Go back to base 10 for center
-			x_center_10 = 10**(error_data['x_center'])  
-			
-			if 'x_center' in error_data and 'dx_pos' in error_data and 'dx_neg' in error_data:
-				# Go back to base 10 for bounds
-				x_high = 10**(error_data['x_center'] + abs(error_data['dx_pos']))   
-				x_low =  10**(error_data['x_center'] - abs(error_data['dx_neg']))  
-				# Add asymmetric error bars using the transformed values
-				ax.errorbar(
-					x_center_10,  # Plot the log-transformed central x value
-					error_data['y'],
-					xerr=[[x_low], [x_high]],  
-					fmt='o',  # No markers
-					color=tableau20[color_index], 
-					markersize=5,
-					capsize=capsize, 
-					elinewidth=2,
-					capthick=2
-				)
-			elif 'x_center' in error_data:
-				# Draw a small line at the tail with a cap
-				ax.plot([x_center_10, x_center_10], 
-						[error_data['y'] * 1.4, error_data['y'] / 1.4], 
-						color=tableau20[color_index], 
-						lw=2)  # Tail line with a round cap
-				# Draw arrow for upper bound only without legend label
-				ax.annotate('', 
-							xy=(x_center_10 * 1.1, error_data['y']), 
-							xytext=(x_center_10 * 0.15, error_data['y']),
-							arrowprops=dict(
-								arrowstyle='<|-',  
-								lw=2,               # Line width
-								color=tableau20[color_index],  # Arrow color
-								mutation_scale=16,   # Size of the arrow
-							)
-				)
+            # Go back to base 10 for center
+            x_center_10 = 10**(error_data['x_center'])
+            
+            if 'x_center' in error_data and 'dx_pos' in error_data and 'dx_neg' in error_data:
+                # Go back to base 10 for bounds
+                x_high = 10**(error_data['x_center'] + abs(error_data['dx_pos']))
+                x_low =  10**(error_data['x_center'] - abs(error_data['dx_neg']))
+                # Add asymmetric error bars using the transformed values
+                ax.errorbar(
+                    x_center_10,  # Plot the log-transformed central x value
+                    error_data['y'],
+                    xerr=[[abs(x_center_10-x_low)], [abs(x_center_10-x_high)]],
+                    fmt='o',  # No markers
+                    color=tableau20[color_index],
+                    markersize=5,
+                    capsize=capsize,
+                    elinewidth=2,
+                    capthick=2
+                )
+            elif 'x_center' in error_data:
+                # Draw a small line at the tail with a cap
+                ax.plot([x_center_10, x_center_10],
+                        [error_data['y'] * 1.4, error_data['y'] / 1.4],
+                        color=tableau20[color_index],
+                        lw=2)  # Tail line with a round cap
+                # Draw arrow for upper bound only without legend label
+                ax.annotate('',
+                        xy=(x_center_10 * 1.1, error_data['y']),
+                        xytext=(x_center_10 * 0.15, error_data['y']),
+                        arrowprops=dict(
+                            arrowstyle='<|-',
+                            lw=2,               # Line width
+                            color=tableau20[color_index],  # Arrow color
+                            mutation_scale=16,   # Size of the arrow
+                        )
+                )
 
-	ax.set_xscale('log')
-	ax.set_yscale('log')
-	ax.invert_yaxis()
-	ax.set_xlim(limits['x_min'], limits['x_max'])
-	ax.set_ylim(limits['y_min'], limits['y_max'])
-	ax.legend(frameon=0, prop={'size': 10}, loc='best')
-	ax.set_title(title)
-	ax.set_xlabel("Mixing Ratio")
-	ax.set_ylabel("Pressure (bar)")
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.invert_yaxis()
+    ax.set_xlim(limits['x_min'], limits['x_max'])
+    ax.set_ylim(limits['y_min'], limits['y_max'])
+    ax.legend(frameon=0, prop={'size': 10}, loc='best')
+    ax.set_title(title)
+    ax.set_xlabel("Mixing Ratio")
+    ax.set_ylabel("Pressure (bar)")
 
 # Save as PNG output file
 output_path = os.path.join(plot_dir, plot_name + '.png')
--- vulcan_cfg.py
+++ vulcan_cfg.py
@@ -16,12 +16,12 @@ atm_file = 'atm/atm_K218b_Hy_WoganPT_10bar_plus70K_forTOI.txt' # TP and Kzz (opt
 sflux_file = 'atm/stellar_flux/sflux-GJ163.txt' # sflux-HD189_B2020.txt This is the flux density at the stellar surface
 top_BC_flux_file = 'atm/BC_top.txt' # the file for the top boundary conditions
 bot_BC_flux_file = 'atm/BC_bot_K218b_Wogan_life1.txt' # the file for the lower boundary conditions
-vul_ini =  'output/k218_to_toi/VUH_TOI_fullchange_GJ163_nz250_1e11s.vul' # the file to initialize the abundances for ini_mix = 'vulcan_ini'
+vul_ini =  '' # the file to initialize the abundances for ini_mix = 'vulcan_ini'
 # output:
-output_dir = 'output/k218_to_toi/'
+output_dir = 'output/TOI_isothermal/'
 plot_dir = 'plot/'
 movie_dir = 'plot/movie/'
-out_name =  'VUH_TOI_fullchange_GJ163_nz250_1e11s.vul' # output file name
+out_name =  '305iso_uninhabited_5e5kzz_nz250_1e17s.vul' # output file name
 
 # ====== For Long runs ======
 use_finalsmooth = 0
@@ -39,7 +39,7 @@ He_H = 0.09692
 ini_mix = 'const_mix' # Options: 'EQ', 'const_mix', 'vulcan_ini', 'table' (for 'vulcan_ini, the T-P grids have to be exactly the same)
 fastchem_met_scale =  1 * factor # scaling factor for other elements in fastchem (e.g., if fastchem_met_scale = 0.1, other elements such as Si and Mg will take 0.1 solar values)
 
-# Initialsing uniform (constant with pressure) mixing ratios (only reads when ini_mix = const_mix)
+# Initialising uniform (constant with pressure) mixing ratios (only reads when ini_mix = const_mix)
 const_mix = {'CH4':1e-10, 'CO':1e-10, 'CO2': 0.008, 'O2':O_H*0.01, 'He':0.09691, 'H2O':0.1, 'N2':0.003, 'H2':0.85}
 
 # ====== Setting up photochemistry ======
@@ -78,13 +78,13 @@ P_t = 1e6 * 1e-11 # pressure at the top (dyne/cm^2)
 use_Kzz = True
 use_moldiff = True
 use_vz = False
-atm_type = 'file'  # Options: 'isothermal', 'analytical', 'file', or 'vulcan_ini' 'table'
+atm_type = 'isothermal'  # Options: 'isothermal', 'analytical', 'file', or 'vulcan_ini' 'table'
 Kzz_prof = 'const' # Options: 'const','file' or 'Pfunc' (Kzz increased with P^-0.4)
 K_max = 5e5        # for Kzz_prof = 'Pfunc'
 K_p_lev = 0.1      # for Kzz_prof = 'Pfunc'
 vz_prof = 'const'  # Options: 'const' or 'file'
 gs = 1031.66         # surface gravity (cm/s^2)  (HD189:2140  HD209:936)
-Tiso = 315 # only read when atm_type = 'isothermal'
+Tiso = 305.0 # only read when atm_type = 'isothermal'
 # setting the parameters for the analytical T-P from (126)in Heng et al. 2014. Only reads when atm_type = 'analytical'
 # T_int, T_irr, ka_L, ka_S, beta_S, beta_L
 para_warm = [120., 1500., 0.1, 0.02, 1., 1.]
@@ -116,7 +116,7 @@ use_condense = True
 use_settling = True
 start_conden_time = 0
 humidity = 1.0
-stop_conden_time = 1e7 # after this time to fix the condensable species, if wrong try 5e7
+stop_conden_time = 4e7 # after this time to fix the condensable species, if wrong try 5e7. This has now been increase to 1e9 after Winter
 condense_sp = ['H2O']
 non_gas_sp = ['H2O_l_s']
 r_p = {'H2O_l_s': 5e-3}  # particle radius in cm (1e-4 = 1 micron)
@@ -137,14 +137,14 @@ use_print_delta = False
 print_prog_num = 500  # print the progress every x steps
 dttry = 1.E-10
 trun_min = 1e2
-runtime = 1e11
+runtime = 1e17
 dt_min = 1.E-10
 #dt_max = 5e13
 dt_max = 1e14
 dt_var_max = 2.
 dt_var_min = 0.5
 count_min = 120
-count_max = 10000
+count_max = 20000
 atol = 1.E0 # Try decreasing this if the solutions are not stable
 mtol = 1e-17 #1.E-20
 mtol_conv = 1e-13 #1.E-18
